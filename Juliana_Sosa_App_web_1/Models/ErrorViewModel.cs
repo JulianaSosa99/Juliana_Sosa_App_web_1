743dcc8983e29defde4857e422440d0ea47a2e41
laboratorio <laboratorio@UPEAULA-31715>
2024-04-08 10:44:55 -0500

    Agregar archivos de proyecto.

---
+++ Juliana_Sosa_App_web_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Juliana_Sosa_App_web_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
